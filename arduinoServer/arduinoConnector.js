dd219e5347859db66debf538cce33eafab3e44e1
Pofat Tseng <tjazzter@gmail.com>
2016-07-22 14:16:14 +0800

    feat: socket.IO event handler

--- arduinoServer/arduinoConnector.js
+++ arduinoServer/arduinoConnector.js
@@ -1,10 +1,12 @@
 var net = require('net');
 var firmata = require('firmata');
 var five = require('johnny-five');
+var socketIO = require('socket.io-client')('SOCKET.IO_SERVER_URL');
 
 var socketClient = null;
 var firmataIO = null;
 var board = null;
+var led = null;
 
 /* var for temperary cache */
 var currentTemperature = 0.0;
@@ -41,6 +43,8 @@ client.on('connect', function(){
 
     board.on('ready', function(){
         console.log('johnny-five is ready');
+
+        led = five.Led(13);
         var multi = new five.Multi({
           controller: "HTU21D"
         });
@@ -48,10 +52,49 @@ client.on('connect', function(){
         multi.on("change", function() {
           // acquire current humidity and temperature
           currentTemperature = this.temperature.celsius;
-          currentHumidity = this.hygrometer.relativeHumidity;
+          currentHumidity = 100 + this.hygrometer.relativeHumidity;
         });
 
     });
 
   });
+});
+
+/* socket.IO */
+socketIO.on('connect', function(){
+  console.log("Connected to Socket.IO server");
+});
+
+/* led event */
+socketIO.on('led_on', function() {
+  if(led) { led.on(); }
+
+});
+
+socketIO.on('led_off', function() {
+  if(led) { led.off(); }
+});
+
+socketIO.on('led_blink', function() {
+  if(led) { led.blink(500); }
+});
+
+// return corresponding values of sensor
+socketIO.on('query', function(field, fn){
+  switch (field) {
+    case 'temp':
+      fn(currentTemperature);
+      break;
+    case 'humid':
+      fn(currentHumidity);
+      break;
+    case 'all':
+      var data = new Object();
+      data.temp = currentTemperature;
+      data.humid = currentHumidity;
+      fn(data);
+      break;
+    default:
+      fn(null);
+  }
 });
